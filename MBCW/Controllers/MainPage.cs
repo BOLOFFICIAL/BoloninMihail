52544c7f21947248e2cce77f1943f4bcd56b29e7
Bolonin  Official <boloninnn@gmail.com>
2023-02-20 20:44:53 +0300

    Добавлено фото и список ссылок

---
+++ MBCW/Controllers/MainPage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MBCW.Controllers
+{
+    public class MainPage : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
